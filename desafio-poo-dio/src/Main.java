57578a879eed076ddccdb17e194453466914f9e8
Emerson <emersonsads@icloud.com>
2024-05-22 20:13:16 -0300

    Desafio POO DIO
    Signed-off-by: Emerson <emersonsads@icloud.com>

---
+++ desafio-poo-dio/src/Main.java
@@ -0,0 +1,45 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição Curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Emerson");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + dev.getConteudosInscritos());
+        dev.progredir();
+        dev.progredir();
+        dev.progredir();
+        System.out.println("=".repeat(200));
+        System.out.println("Conteúdos Inscritos " + dev.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " + dev.getConteudosConcluidos());
+        System.out.println("XP" + dev.calcularXP());
+
+    }
+}
